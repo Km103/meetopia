a48b4eb89331dba25b162a5dfe935ef275336d07
Km103 <21ucs103@lnmiit.ac.in>
2024-04-14 00:14:02 +0530

    added resend,types

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,30 @@
+import { resend } from "@/lib/resend"
+import VerificationEmail from "../../emails/verificationEmail"
+
+import { ApiResponse } from "@/types/ApiResponse"
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "onboard@resend.dev",
+      to: email,
+      subject: "Verify your email",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    })
+
+    return {
+      success: true,
+      message: "Verification email sent",
+    }
+  } catch (error) {
+    console.error("error sending verification email: ", error)
+    return {
+      success: false,
+      message: "Failed to send verification email",
+    }
+  }
+}
