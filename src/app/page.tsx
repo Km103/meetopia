a96a3165d3a1768e197e379d2f0c0784ec28beed
shanatanu002 <bajpaishantanu2002@gmail.com>
2024-04-14 15:44:46 +0530

    This website is under development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl text-gray-100 text-center py-16">This website is under Development.</h1>
+      <h1 className="text-3xl text-gray-100 text-center py-16">This website is under development.</h1>
     </div>
   )
 }
